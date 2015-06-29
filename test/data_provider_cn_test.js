6c3335604bc1e8382957cf5f37a19bba1ca04129
waitfish <dwj_zz@163.com>
2015-06-29 20:57:32 +0800

    添加了新的provider identifyNum 方法

--- test/data_provider_cn_test.js
+++ test/data_provider_cn_test.js
@@ -7,18 +7,22 @@ var expect = require('chai').expect
 
 describe('#data_provider_cn Test', function () {
     it('不传入性别，生成随机中文名', function () {
-        var name = providers.name_cn()
+        var name = providers.name_cn();
         console.log(name)
     })
     it('name_cn()', function () {
-        expect(typeof(providers.name_cn())).equal('string')
-        expect(typeof(providers.name_cn('f'))).equal('string')
+        expect(typeof(providers.name_cn())).equal('string');
+        expect(typeof(providers.name_cn('f'))).equal('string');
         expect(typeof(providers.name_cn('m'))).equal('string')
 
     });
 
     it('address_cn()', function () {
-        expect(typeof(providers.address_cn('杭州'))).equal('string')
+        expect(typeof(providers.address_cn('杭州'))).equal('string');
         expect(typeof(providers.address_cn())).equal('string')
+    });
+
+    it('identifyNum()', function () {
+        expect(typeof(providers.identityNum())).equal('string');
     })
 })
